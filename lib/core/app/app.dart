fb6d1628a716303ed83a2a123d5e93eb408a1f16
michael kalango <michaelkalango@gmail.com>
2022-11-17 06:58:51 +0100

    feat: database service

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,7 +1,9 @@
+import 'package:sqflite_migration_service/sqflite_migration_service.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import '../../features/features.dart';
+import '../services/services.dart';
 
 @StackedApp(
   routes: [
@@ -12,6 +14,8 @@ import '../../features/features.dart';
   dependencies: [
     LazySingleton(classType: NavigationService),
     LazySingleton(classType: DialogService),
+    LazySingleton(classType: DatabaseMigrationService),
+    LazySingleton(classType: DatabaseService)
   ],
   logger: StackedLogger(),
 )
